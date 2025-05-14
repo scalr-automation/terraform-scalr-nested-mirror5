023216735f4eabac71f639553d0f026dcc8f6f64
Roman Drahunov <104847647+RomanDrahunovScalr@users.noreply.github.com>
2025-05-14 11:41:13 +0300

    Update test.tf

--- null_resource/test.tf
+++ null_resource/test.tf
@@ -2,7 +2,7 @@ resource "null_resource" "resource1" {
   provisioner "local-exec" {
     command = "env"
     environment = {
-      ENV = "Hello World!"
+      ENV = "Hello World2!"
     }
   }
   
